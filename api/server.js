acd9c4e68feee04df420621cfd55a302fa9ab213
Tristan H <tristanskyhook@gmail.com>
2024-06-10 19:52:26 -0700

    rate limit

--- api/server.js
+++ api/server.js
@@ -13,58 +13,19 @@ app.use(bodyParser.json());
 module.exports.app = app;
 const port = process.env.PORT;
 
-//redis
-const redis = require("redis");
-const client = redis.createClient({
-  host: "localhost", // Docker container running on the same host
-  port: 6379,
-});
+// Rate limiting
+const rateLimit = require('express-rate-limit')
 
-client.on("error", (err) => {
-  console.error("Redis error:", err);
-});
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
-const rateLimitMiddleware = async (req, res, next) => {
-  const ip = req.ip;
-  const bucketSize = 10; // Maximum number of tokens in the bucket
-  const refillRate = 1; // Tokens added per second
-  const refillInterval = 1000; // Refill interval in milliseconds
-  const hashKey = `rate_limit:${ip}`;
-
-  const currentTime = Date.now();
-  const hashData = await client.hgetall(hashKey);
-
-  let tokens;
-  let lastRefillTime;
-
-  if (hashData && Object.keys(hashData).length > 0) {
-    tokens = parseInt(hashData.tokens);
-    lastRefillTime = parseInt(hashData.lastRefillTime);
-  } else {
-    tokens = bucketSize;
-    lastRefillTime = currentTime;
-  }
-
-  // Calculate the number of tokens to add based on elapsed time
-  const elapsedTime = (currentTime - lastRefillTime) / refillInterval;
-  const newTokens = Math.min(
-    bucketSize,
-    tokens + Math.floor(elapsedTime * refillRate)
-  );
-  tokens = newTokens;
-  lastRefillTime =
-    currentTime - ((currentTime - lastRefillTime) % refillInterval);
-
-  if (tokens > 0) {
-    // Consume a token
-    tokens -= 1;
-    await client.hset(hashKey, "tokens", tokens);
-    await client.hset(hashKey, "lastRefillTime", lastRefillTime);
-    next();
-  } else {
-    res.status(429).send("Too many requests, please try again later.");
-  }
-};
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 //json web token
 const jwt = require("jsonwebtoken");
@@ -89,7 +50,6 @@ const jwtVerificationMiddleware = async (req, res, next) => {
 };
 
 app.use(jwtVerificationMiddleware); //keep first if we only allow loged in users to do anything
-app.use(rateLimitMiddleware); //make first if we want to allow not logged in users, but still limit
 
 // add our routes
 require("./routes/users")(app);
